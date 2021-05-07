6b1340618f3ddaa38dc4ea5d106ef1109cab123e
weimou <weimoumw@gmail.com>
2021-05-06 23:28:02 -0700

    clean

--- tools/carcontrols/keyboardd.py
+++ tools/carcontrols/keyboardd.py
@@ -16,17 +16,6 @@ current_desire = DesireStatus.laneFollow
 class MyException(Exception):
   pass
 
-# def on_press(key):
-#   try:
-#     print('alphanumeric key {0} pressed'.format(key.char))
-#     if key.char == 'l':
-#       print('left turn')
-#     if key.char == 'q' or key == keyboard.Key.esc:
-#       raise MyException(key)
-      
-#   except AttributeError:
-#     print('special key {0} pressed'.format(key))
-
 with keyboard.Events() as events:
   # Block at most one second
   keyboard_sock = messaging.pub_sock('testJoystick')
@@ -54,80 +43,3 @@ with keyboard.Events() as events:
     dat.testJoystick.desire = current_desire
     print(dat)
     keyboard_sock.send(dat.to_bytes())
-      
-#   except AttributeError:
-#     print('special key {0} pressed'.format(key))
-      # if key_pressed.char == 'l':
-      #   print('left turn turn turn')
-    # print()
-    # if event is None:
-    #     print('You did not press a key within one second')
-    # else:
-    #     print('Received event {}'.format(event))
-
-# Collect events until released
-# with keyboard.Listener(
-#         on_press=on_press) as listener:
-#     print("listening")
-#     try:
-#         listener.join()
-#     except MyException as e:
-#         print('{0} was pressed'.format(e.args[0]))
-
-# def keyboard_thread():
-#   keyboard_sock = messaging.pub_sock('testJoystick')
-
-#   # pygame.init()
-
-#   # # Used to manage how fast the screen updates
-#   # clock = pygame.time.Clock()
-
-#   # # Initialize the joysticks
-#   # pygame.joystick.init()
-
-#   # # Get count of joysticks
-#   # joystick_count = pygame.joystick.get_count()
-#   # if joystick_count > 1:
-#   #   raise ValueError("More than one joystick attached")
-#   # elif joystick_count < 1:
-#   #   raise ValueError("No joystick found")
-
-#   # -------- Main Program Loop -----------
-#   while True:
-#     # EVENT PROCESSING STEP
-#     # for event in pygame.event.get():  # User did something
-#     #   if event.type == pygame.QUIT:  # If user clicked close
-#     #     pass
-#     #   # Available joystick events: JOYAXISMOTION JOYBALLMOTION JOYBUTTONDOWN JOYBUTTONUP JOYHATMOTION
-#     #   if event.type == pygame.JOYBUTTONDOWN:
-#     #     print("Joystick button pressed.")
-#     #   if event.type == pygame.JOYBUTTONUP:
-#     #     print("Joystick button released.")
-
-#     # joystick = pygame.joystick.Joystick(0)
-#     # joystick.init()
-
-#     # # Usually axis run in pairs, up/down for one, and left/right for
-#     # # the other.
-#     # axes = []
-#     # buttons = []
-
-#     # for a in range(joystick.get_numaxes()):
-#     #   axes.append(joystick.get_axis(a))
-
-#     # for b in range(joystick.get_numbuttons()):
-#     #   buttons.append(bool(joystick.get_button(b)))
-#     if keyboard.is_pressed('q'):  # if key 'q' is pressed 
-#         print('You Pressed A Key!')
-#         break  # finishing the loop
-
-#     dat = messaging.new_message('testJoystick')
-#     # dat.testJoystick.axes = axes
-#     # dat.testJoystick.buttons = buttons
-#     keyboard_sock.send(dat.to_bytes())
-
-#     # Limit to 10 frames per second
-#     sleep(0.1)
-
-# if __name__ == "__main__":
-#   keyboard_thread()
