9bd62a0be90034507269531ca96c358ac36fea23
weimou <weimoumw@gmail.com>
2021-05-31 15:37:12 -0700

    update keyboard.py and remove extra dependency

--- tools/carcontrols/keyboardd.py
+++ tools/carcontrols/keyboardd.py
@@ -7,39 +7,71 @@
 ### this process needs pygame and can't run on the EON ###
 
 # import pygame  # pylint: disable=import-error
+
 import cereal.messaging as messaging
 from cereal import log
-from pynput import keyboard
+
+import sys
+import termios
+import time
+from termios import (BRKINT, CS8, CSIZE, ECHO, ICANON, ICRNL, IEXTEN, INPCK,
+                     ISTRIP, IXON, PARENB, VMIN, VTIME)
+from typing import Any
+import select
+
+# Indexes for termios list.
+IFLAG = 0
+OFLAG = 1
+CFLAG = 2
+LFLAG = 3
+ISPEED = 4
+OSPEED = 5
+CC = 6
+
+def getch():
+  fd = sys.stdin.fileno()
+  old_settings = termios.tcgetattr(fd)
+  try:
+    # set
+    mode = termios.tcgetattr(fd)
+    mode[IFLAG] = mode[IFLAG] & ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON)
+    #mode[OFLAG] = mode[OFLAG] & ~(OPOST)
+    mode[CFLAG] = mode[CFLAG] & ~(CSIZE | PARENB)
+    mode[CFLAG] = mode[CFLAG] | CS8
+    mode[LFLAG] = mode[LFLAG] & ~(ECHO | ICANON | IEXTEN)
+    mode[CC][VMIN] = 1
+    mode[CC][VTIME] = 0
+    termios.tcsetattr(fd, termios.TCSAFLUSH, mode)
+
+    i, _, _ = select.select( [sys.stdin], [], [], 0.1)
+    if (i):
+      ch = sys.stdin.read(1)
+    else:
+      ch = ""
+  finally:
+    termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+  return ch
+
 
 DesireStatus = log.Joystick.DesireStatus
 current_desire = DesireStatus.laneFollow
-class MyException(Exception):
-  pass
-
-with keyboard.Events() as events:
-  # Block at most one second
-  keyboard_sock = messaging.pub_sock('testJoystick')
-  while True:
-    # reset commit status
-    dat = messaging.new_message('testJoystick')
-    dat.testJoystick.commit = False
-    event = events.get(0.1)
-    if event:
-      key = event.key
-      try:
-        # print('alphanumeric key {0} pressed'.format(key.char))
-        if key.char == 'l':
-          current_desire = DesireStatus.leftLaneChange
-        elif key.char == 'r':
-          current_desire = DesireStatus.rightLaneChange
-        elif key.char == 'f':
-          current_desire = DesireStatus.laneFollow
-        elif key.char == 'c':
-          dat.testJoystick.commit = True
-        elif key == keyboard.Key.esc:
-          raise MyException(key)
-      except AttributeError:
-        pass
-    dat.testJoystick.desire = current_desire
-    print(dat)
-    keyboard_sock.send(dat.to_bytes())
+keyboard_sock = messaging.pub_sock('testJoystick')
+while True:
+  # reset commit status
+  dat = messaging.new_message('testJoystick')
+  dat.testJoystick.commit = False
+  ch = getch()
+  if ch == 'l':
+    current_desire = DesireStatus.leftLaneChange
+  elif ch == 'r':
+    current_desire = DesireStatus.rightLaneChange
+  elif ch == 'f':
+    current_desire = DesireStatus.laneFollow
+  elif ch == 'c':
+    dat.testJoystick.commit = True
+  else:
+    continue
+  dat.testJoystick.desire = current_desire
+  print(ch)
+  keyboard_sock.send(dat.to_bytes())
+  
